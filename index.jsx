5bdcd575e40721e07cb4c4bc900f9761c64adae1
Robyn011 <robyncocopumpoen@gmail.com>
2024-06-05 12:39:59 +0200

    adding the browser route

--- index.jsx
+++ index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <h1>Hello, React Router!</h1>
   )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);

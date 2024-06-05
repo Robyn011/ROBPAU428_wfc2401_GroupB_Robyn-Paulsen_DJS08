4fcb761751d42f6b79a6e041936ca4622a079d2b
Robyn011 <robyncocopumpoen@gmail.com>
2024-06-05 12:25:37 +0200

    adding BrowserRouter

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
@@ -7,4 +8,8 @@ function App() {
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
